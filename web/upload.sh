755e5a568190e8117ec10ab1146730584a82d64c
Laura Devendorf <ldevendorf@gmail.com>
2014-04-30 11:46:36 -0700

    Delete upload.sh

--- web/upload.sh
+++
@@ -1,6 +0,0 @@
-echo "** this does not upload lib files"
-echo "" 
-scp /Volumes/Storage/btm/gcode-viewer/web/*.js artfordorks:/home/43008/users/.home/domains/artfordorks.com/html/btm
-echo 
-scp /Volumes/Storage/btm/gcode-viewer/web/*.html artfordorks:/home/43008/users/.home/domains/artfordorks.com/html/btm
-
