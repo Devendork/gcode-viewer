0933fe1e488570b929fc09184d63b3c5a37da4b9
Laura Devendorf <ldevendorf@gmail.com>
2014-04-30 11:46:14 -0700

    Delete download.sh

--- web/download.sh
+++
@@ -1,5 +0,0 @@
-echo "** this does not dowload lib files"
-echo "" 
-scp artfordorks:/home/43008/users/.home/domains/artfordorks.com/html/btm/*.js /Volumes/Storage/btm/gcode-viewer/web/ 
-echo 
-scp artfordorks:/home/43008/users/.home/domains/artfordorks.com/html/btm/*.html  /Volumes/Storage/btm/gcode-viewer/web/*.html
